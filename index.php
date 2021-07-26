270cee063b92c87936063b59cee6e4f27fb7fa78
Zhyrk <riki.zancan@gmail.com>
2021-07-26 22:25:24 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <link href="style.css" type="text/css" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta http-equiv="Pragma" content="no-cache" />
+        <meta http-equiv="Expires" content="0" />
+        <script src="core.js"></script>
+    </head>
+    <body>
+    
+        <main></main>
+        <script src="core.js"></script>
+        <script>
+            
+        </script>
+        <script>
+            setVariable("maxPlayers", 10)
+            loadContent("inserisciGiocatori.html")
+            
+        </script>
+
+    </body>
+</html>
