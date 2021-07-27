7d9091d96256d4149e4429c571cdf5a4351cb9b9
Zhyrk <riki.zancan@gmail.com>
2021-07-27 22:08:21 +0200

    Update prova.php

--- prova.php
+++ prova.php
@@ -1,9 +1,10 @@
 <?php
-    echo 'test';
     $db = parse_url(getenv("DATABASE_URL"));
     $db["path"] = ltrim($db["path"], "/");
+
     $conn = pg_connect(getenv("DATABASE_URL"));
-    echo 'prova';
-    $result = pg_query($conn, "select 123 as test");
+    $result = pg_query($conn, "CREATE TABLE Categorie(id_categoria int not null auto_increment PRIMARY KEY, nome varchar(100) not null)");
     var_dump(pg_fetch_all($result));
+
+    pg_close($conn);
 ?>
