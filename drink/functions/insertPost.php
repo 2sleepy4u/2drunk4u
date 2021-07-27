3cb98525d379ee6ca9cabd2f427af6cfe433f10c
Zhyrk <riki.zancan@gmail.com>
2021-07-28 00:15:23 +0200

    Add files via upload

--- drink/functions/insertPost.php
+++ drink/functions/insertPost.php
@@ -1,19 +1,10 @@
 <?php
     include("connect.php");
-    $testo = mysqli_real_escape_string($conn, $_POST['testo']);
-    $categoria = $_POST['category'];
-    $titolo = $_POST['titolo'];
-    $data = date('Y-m-d H:i:s');
-    $query = "INSERT INTO Post (testo, categoria, titolo, data) VALUES ('$testo', '$categoria', '$titolo', '$data')";
+    $testo = $_POST['testo'];
+    $query = "INSERT INTO Post (testo, categoria) VALUES ('$testo', 1)";
+    $result = pg_query($conn, $query);
+    echo pg_fetch_all($result);
 
-    if (mysqli_query($conn, $query)) 
-    {
-        echo "New record created successfully \n";
-    } 
-    else 
-    {
-        echo "Error: " . $query . "<br>" . mysqli_error($conn);
-    }
     include("disconnect.php");
 ?>
 
