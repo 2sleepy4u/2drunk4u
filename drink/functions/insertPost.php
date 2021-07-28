428bfa68fc1476b274181dd16d36c60a0ce14011
Riccardo Zancan <riki.zancan@gmail.com>
2021-07-28 15:42:30 +0200

    Cabmiamenti 2

--- drink/functions/insertPost.php
+++ drink/functions/insertPost.php
@@ -1,13 +1,13 @@
 <?php
     include("connect.php");
-    $testo = pg_escape_string($_POST['testo']);
-    $query = "INSERT INTO Post (testo, categoria) VALUES ('$testo', 1)";
+    //per togliere i caratteri problematici tipo apostrofi
+    $testo = explode(PHP_EOL, $_POST['testo']);
+    $testo = pg_escape_string($testo);
+
+    $query = "INSERT INTO Post (testo, categoria) VALUES ('$testo', 2)";
     $result = pg_query($conn, $query);
     echo pg_fetch_all($result);
 
-    include("disconnect.php");
+    include("disconnect.php");+
+    header("index.php")
 ?>
-
-<div>
-    <a href="index.php"> HOME </a>
-</div>
