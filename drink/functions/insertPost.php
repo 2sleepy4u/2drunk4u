ab47552beb9083b84f2b6fcba3c38cba9b39a02b
Riccardo Zancan <riki.zancan@gmail.com>
2021-07-28 15:57:09 +0200

    Cabmiamenti prova

--- drink/functions/insertPost.php
+++ drink/functions/insertPost.php
@@ -1,16 +1,15 @@
 <?php
     include("connect.php");
     //per togliere i caratteri problematici tipo apostrofi
-    $testo = $_POST['testo'];
-    //$testo = explode(PHP_EOL, $testo);
+    $testo = explode(PHP_EOL, $$_POST['testo']);
     $testo = pg_escape_string($testo);
 
-    $query = "INSERT INTO Post (testo, categoria) VALUES ('$testo', 1)";
+    $query = "INSERT INTO Post (testo, categoria) VALUES ('$testo', 2)";
     $result = pg_query($conn, $query);
     echo pg_fetch_all($result);
 
     include("disconnect.php");
-    //header("index.php")
+    header("index.php")
 ?>
 <div>
     <a href="index.php"> HOME </a>
