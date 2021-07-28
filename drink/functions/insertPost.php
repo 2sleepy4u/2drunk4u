135e4dd345891dd099993589352438092cf871d3
Riccardo Zancan <riki.zancan@gmail.com>
2021-07-28 16:23:44 +0200

    Prova suddivisione righe

--- drink/functions/insertPost.php
+++ drink/functions/insertPost.php
@@ -2,9 +2,10 @@
     include("connect.php");
     //per togliere i caratteri problematici tipo apostrofi
     $testo =  $_POST['testo'];
-    //ci pensa lui agli apostrofi
-    $testo = pg_escape_string($testo);
 
+    $testo = pg_escape_string($testo);
+    $rows = preg_split('/\r\n|\r|\n/', $testo);
+    echo $rows;
     $query = "INSERT INTO Post (testo, categoria) VALUES ('$testo', 2)";
     $result = pg_query($conn, $query);
     echo pg_fetch_all($result);
