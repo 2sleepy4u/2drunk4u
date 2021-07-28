3596321a14996ea11726d9163a7fc617cc5cb5b7
Riccardo Zancan <riki.zancan@gmail.com>
2021-07-28 16:53:58 +0200

    Riordinamento cartelle e file

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <link href="style.css" type="text/css" rel="stylesheet" />
+        <link href="style/style.css" type="text/css" rel="stylesheet" />
         <meta name="viewport" content="width=device-width">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
