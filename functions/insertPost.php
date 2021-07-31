c24abd36ef30f18fc4fc7a663cfc48b818f8a691
Riccardo Zancan <riki.zancan@gmail.com>
2021-07-31 17:49:21 +0200

    Piccola fix

--- functions/insertPost.php
+++ functions/insertPost.php
@@ -19,7 +19,7 @@
     include("disconnect.php");
 ?>
 <script>
-    window.location.replace("https://im2drunk4u.herokuapp.com");
+    //window.location.replace("https://im2drunk4u.herokuapp.com");
 </script>
 <div>
     <a href="https://im2drunk4u.herokuapp.com"> HOME </a>
